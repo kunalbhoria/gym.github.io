c8def0f8f304144177091bc1ed782599a93a0c39
kunalbhoria <75310757+kunalbhoria@users.noreply.github.com>
2021-05-20 13:37:43 +0530

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,27 @@
+
+
+// let userInput = parseInt(`${prompt("Enter the maximum number")}`);
+
+//     if(userInput===NaN){
+//         // userInput = prompt("Enter a valid Number !");
+//         console.log("enter a valid number")
+//     }
+//     else{
+//         console.log(" valid number")
+    
+//     }
+
+
+// randomNumber = Math.floor(Math.random()*userInput) + 1 ;
+// console.log(randomNumber);
+
+// userInput = prompt("Now suggest the number!");
+// while(true){ if(userInput===randomNumber){
+//     alert(" Congrats, You got it !");
+//     break;
+//  }
+//  else{
+//      userInput = prompt("try again !")
+
+//  }
+// break;}
